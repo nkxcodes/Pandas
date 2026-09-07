3fb2767716f294f2267c48f28bd99250cbdb5495
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-07 19:32:40 +0530

    Update DataFrame script to include printing the first 10 rows and improve formatting of print statements

--- pandas-learning/basic_dataframe/001_pandas.py
+++ pandas-learning/basic_dataframe/001_pandas.py
@@ -7,5 +7,8 @@ df = pd.read_csv("pandas-learning/data/students.csv")
 # Q3: Print the entire DataFrame
 print(df)
 
-# Q4 Print the first five rows
+# Q4: Print the first five rows
 print(df.head())
+
+# Q5: Print the first 10 rows.
+print(df.head(10))
