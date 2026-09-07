9931131585d527f7be908847b2781cc98fc2affd
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-07 19:25:38 +0530

    Add a new script to import pandas and read students data into a DataFrame

---
+++ pandas-learning/basic_dataframe/001_pandas.py
@@ -0,0 +1,8 @@
+# Q1: Import pandas
+import pandas as pd
+
+# Q2: Read students.csv into a DataFrame
+df = pd.read_csv("pandas-learning/data/students.csv")
+
+# Q3: Print the entire DataFrame
+print(df)
