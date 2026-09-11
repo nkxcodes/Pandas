a8fe570e61f84e8811f92fd485e951bc77c72c96
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-11 18:42:20 +0530

    Add functionality to select and print the second row of the DataFrame using iloc, in addition to the first row.

--- pandas-learning/basic_dataframe/003_selecting_rows_with_iloc.py
+++ pandas-learning/basic_dataframe/003_selecting_rows_with_iloc.py
@@ -3,4 +3,9 @@ import pandas as pd
 df = pd.read_csv("pandas-learning/data/students.csv")
 
 # Q36: Select the first row using iloc.
+print()
 print(df.iloc[0])
+
+# Q37: Select the second row.
+print()
+print(df.iloc[1])
