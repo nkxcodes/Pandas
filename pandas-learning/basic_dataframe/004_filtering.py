ce077535aa21396081c499823b4b06daba9d2d89
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 12:53:07 +0530

    Add functionality to filter and print students with marks less than 50 from the DataFrame, enhancing data selection capabilities.

--- pandas-learning/basic_dataframe/004_filtering.py
+++ pandas-learning/basic_dataframe/004_filtering.py
@@ -4,4 +4,8 @@ df = pd.read_csv('pandas-learning/data/students.csv')
 
 # Q61: Find the students whose marks are greater than 80.
 print()
-print(df[df['marks'] > 80]) 
+print(df[df['marks'] > 80])
+
+# Q62: Find students whose marks are less than 50.
+print()
+print(df[df['marks'] < 50])
